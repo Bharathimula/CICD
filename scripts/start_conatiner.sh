17de17878f1494a6b391bb64c8210cf51a32f943
Bharathimula <127927331+Bharathimula@users.noreply.github.com>
2025-03-29 11:31:47 +0530

    change for run

--- scripts/start_conatiner.sh
+++ scripts/start_conatiner.sh
@@ -5,4 +5,4 @@ set -e
 docker pull bharu2703/sample-demo-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 haru2703/sample-demo-app
+docker run -d -p 5000:5000 bharu2703/sample-demo-app
